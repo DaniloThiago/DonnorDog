53923c63c18e2a0efb4d92acf7f22ce51fc2603d
DaniloThiago <danilo.t.a.oliveira@hotmail.com>
2017-04-02 23:15:28 -0300

    export mais de um elemento e um default

--- ex/index.jsx
+++ ex/index.jsx
@@ -1,6 +1,13 @@
-import './estilo.css'
+import './estilo'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Component from './component'
 
-ReactDOM.render(<Component value="Show!" />, document.getElementById('app'))
+import ComponentDefault, { Component1, Component2 } from './component' // caso tenha mais de um componente dentro do arquivo e tenha definido um como default
+
+ReactDOM.render(
+	<div>
+		<ComponentDefault value="Default" />
+		<Component1 value="Component1" />
+		<Component2 value="Component2" />
+	</div>, document.getElementById('app'))
