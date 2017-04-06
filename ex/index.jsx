860de406d5106bc87e453b5b0850e0e0c788fbb4
Danilo Thiago <danilo.t.a.oliveira@hotmail.com>
2017-04-06 15:51:52 -0300

    teste de componente baseado em classe

--- ex/index.jsx
+++ ex/index.jsx
@@ -3,17 +3,19 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import Status from './status'
 import Dog from './dog'
+import  ClassComponent from './classComponent'
 
 ReactDOM.render(
 	<div>
-	<Status categoria='Doadoras'>
-		<Dog name='Lola' lastName="Habilitada"/>
-		<Dog name='Nina' lastName="Repouso"/>
-	</Status>
-	<Status categoria='Receptor'>
-		<Dog name='Toby' lastName="Urgente"/>
-		<Dog name='Pingo' lastName="Grave"/>
-		<Dog name='Casca' lastName="Risco"/>
-	</Status>
+		<ClassComponent value="Teste Class Componet" />
+		<Status categoria='Doadoras'>
+			<Dog name='Lola' lastName="Habilitada"/>
+			<Dog name='Nina' lastName="Repouso"/>
+		</Status>
+		<Status categoria='Receptor'>
+			<Dog name='Toby' lastName="Urgente"/>
+			<Dog name='Pingo' lastName="Grave"/>
+			<Dog name='Casca' lastName="Risco"/>
+		</Status>
 	</div>
 	, document.getElementById('app'))
