cdf5b65d4fba3fcaf02c7ca7bfd900548a8eba5c
Danilo Thiago <danilo.t.a.oliveira@hotmail.com>
2017-04-06 16:24:23 -0300

    adicionando function

--- ex/index.jsx
+++ ex/index.jsx
@@ -1,21 +1,8 @@
 import './estilo'
 import React from 'react'
 import ReactDOM from 'react-dom'
-import Status from './status'
-import Dog from './dog'
 import  ClassComponent from './classComponent'
 
 ReactDOM.render(
-	<div>
-		<ClassComponent value="Teste Class Componet" />
-		<Status categoria='Doadoras'>
-			<Dog name='Lola' lastName="Habilitada"/>
-			<Dog name='Nina' lastName="Repouso"/>
-		</Status>
-		<Status categoria='Receptor'>
-			<Dog name='Toby' lastName="Urgente"/>
-			<Dog name='Pingo' lastName="Grave"/>
-			<Dog name='Casca' lastName="Risco"/>
-		</Status>
-	</div>
+	<ClassComponent label="Contador" initialValue={10} />
 	, document.getElementById('app'))
