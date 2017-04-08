4aa2f712617d3a762e60ca37bf800e20643c9f11
DaniloThiago <danilo.t.a.oliveira@hotmail.com>
2017-04-08 14:14:42 -0300

    legendas

--- ex/index.jsx
+++ ex/index.jsx
@@ -4,18 +4,21 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import Cards from './cards'
 import Nav from './nav'
+import Legendas from './legendas'
 import { navAction } from '../utils/reactUtils'
 
 let cards = [];
 
 firebase.database().ref('dogs').on('value', function(snapshot) {
+	cards = []
 	snapshot.forEach(function (item) {
 		cards.push(item.val())
 	});
 	ReactDOM.render(
 			<div>
+				<Legendas />
 				<Nav />
-				<div style={{"marginTop":"180px"}}>
+				<div style={{"marginTop":"20px"}}>
 					<Cards cards={cards} />
 				</div>
 			</div>
